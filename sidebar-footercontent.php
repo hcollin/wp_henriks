d3461cdfa25efea09bee073aac6986c7a28f780b
Henrik Collin <Henrik Collin>
2016-07-26 15:33:43 +0300

    Initial commit

---
+++ sidebar-footercontent.php
@@ -0,0 +1,13 @@
+				<div id="footercontent" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
+
+					<?php if ( is_active_sidebar( 'footercontent' ) ) : ?>
+
+						<?php dynamic_sidebar( 'footercontent' ); ?>
+
+					<?php else : ?>
+
+					
+
+					<?php endif; ?>
+
+				</div>
---
+++ sidebar-sidebarmenu.php
@@ -0,0 +1,13 @@
+				<div id="sidebarmenu" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
+
+					<?php if ( is_active_sidebar( 'sidebarmenu' ) ) : ?>
+
+						<?php dynamic_sidebar( 'sidebarmenu' ); ?>
+
+					<?php else : ?>
+
+					
+
+					<?php endif; ?>
+
+				</div>
